04114e26d7aafe5b52355770f8f3872713461f1a
yotam-biu <nagaryo3@biu.ac.il>
2026-01-04 19:02:31 +0200

    Update fire_test.py

--- .tests/fire_test.py
+++ .tests/fire_test.py
@@ -1,17 +1,20 @@
 import json
+import numpy as np
 from spread_fire import spread_fire
 
 def test_fire():
   
   with open('.tests/initial_state.json', 'r') as f:
       grid = json.load(f)
+      grid = np.array(grid)
   
   with open('.tests/final_state.json', 'r') as f:
       final_grid = json.load(f)
+      final_grid = np.array(final_grid)
   
   for i in range(100):
       update_grid = spread_fire(grid)
-      if update_grid == grid:
+      if np.array_equal(update_grid, grid):
           break
       grid = update_grid
   
